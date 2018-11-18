7d941b309232ea645fc260bf52bfe33704f7211a
Daniel Montgomery <d.montgrmry@gmail.com>
2018-11-18 10:53:58 -0800

    Refactor sorting algorithm

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,7 @@
 """
 import re, logging
 
+
 class FileError(Exception):
     pass
 
@@ -14,9 +15,28 @@ class FileIOError(IOError):
 class UnsupportedFiletypeError(FileError):
     pass
 
+
 class NotEnoughFilesError(FileError):
     pass
 
+
+class Pair:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def __str__(self):
+        return '{}, {}'.format(self.a, self.b)
+
+    def __getitem__(self, key):
+        if not isinstance(key, int):
+            raise TypeError('`key` must be of type integer.')
+        if key not in [0, 1]:
+            raise IndexError('Expecting an index of 0 and 1, but found {}.'.format(key))
+
+        if key == 0:
+            return self.a
+        return self.b
 
 def lineify_xml(path, encoding='utf-8'):
     """
@@ -51,3 +71,14 @@ def lineify_xml(path, encoding='utf-8'):
             current_tell = xml.tell()
             content = xml.read(_BLOCK_SIZE).decode(encoding)'''
     _logger.debug('========== END `%s::lineify_xml` ==========', __name__)
+
+
+def sdv(d, reverse=False):
+    """Sort a dictionary by value and return a representation
+    of it as a list.
+
+    :param d: the dictionary to sort
+    :param reverse: whether to reverse the order. Default is false.
+    :returns: a sorted list.
+    """
+    return sorted(d.items(), key=lambda t: t[1], reverse=reverse)
